fc59a9aa171005591e1270138b1ab6c1594178ab
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-01-08 09:56:30 +0700

    fix: binding dashboard

--- lib/app/modules/dashboard/bindings/dashboard_binding.dart
+++ lib/app/modules/dashboard/bindings/dashboard_binding.dart
@@ -1,12 +1,16 @@
 import 'package:get/get.dart';
+import 'package:icp_gahara_mobile/app/modules/home/controllers/home_controller.dart';
+import 'package:icp_gahara_mobile/app/modules/settings/controllers/settings_controller.dart';
 
 import '../controllers/dashboard_controller.dart';
 
 class DashboardBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<DashboardController>(
-      () => DashboardController(),
+    Get.put<DashboardController>(
+      DashboardController(),
     );
+    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<SettingsController>(() => SettingsController());
   }
 }
