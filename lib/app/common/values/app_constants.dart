baf8b8ecacd04d8be88c88b1e662a220eefdc635
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-01-13 08:48:28 +0700

    update: host IP

--- lib/app/common/values/app_constants.dart
+++ lib/app/common/values/app_constants.dart
@@ -1,7 +1,7 @@
 abstract class AppConstants {
   static String token = "token";
-  static String baseAPIURL = "http://202.83.122.66:60613/api/v1";
-  static String baseURL = "http://202.83.122.66:60613/";
+  static String baseAPIURL = "http://202.83.122.66:63523/api/v1";
+  static String baseURL = "http://202.83.122.66:63523/";
 
   static String roleAdmin = "admin";
   static String roleUser = "user";
