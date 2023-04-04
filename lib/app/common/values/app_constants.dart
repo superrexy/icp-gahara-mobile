3a2b40efdf848325c304df45cdaa0eaa7c7b22b9
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-04-04 22:40:01 +0700

    update: base API URL

--- lib/app/common/values/app_constants.dart
+++ lib/app/common/values/app_constants.dart
@@ -1,7 +1,7 @@
 abstract class AppConstants {
   static String token = "token";
-  static String baseAPIURL = "http://10.0.2.2:3000/api/v1";
-  static String baseURL = "http://10.0.2.2:3000/";
+  static String baseAPIURL = "http://103.76.129.90:40102/api/v1";
+  static String baseURL = "http://103.76.129.90:40102/";
 
   static String roleAdmin = "admin";
   static String roleUser = "user";
